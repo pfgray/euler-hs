e072b35560bd9768cc48a5714026ea7f281f1ecb
Paul Gray <pfbgray@gmail.com>
2020-01-15 22:18:32 -0500

    Adding Problem 2 solution

---
+++ 2/2.hs
@@ -0,0 +1,10 @@
+sumEvenFibs :: (Int, Int, Int) -> Int
+sumEvenFibs (prevPrev, prev, acc) =
+  let next = prevPrev + prev in
+    if next > 4000000
+      then acc
+      else if next `mod` 2 == 0
+        then sumEvenFibs (prev, next, acc + next)
+        else sumEvenFibs (prev, next, acc)
+
+main = putStrLn (show (sumEvenFibs (1, 2, 2)))
