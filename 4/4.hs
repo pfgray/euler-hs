6723d2dca4a9be8bbb1893e948f344dd6e47e25e
Paul Gray <pfbgray@gmail.com>
2020-01-18 01:23:54 -0500

    Adding solution to problem #4

---
+++ 4/4.hs
@@ -0,0 +1,24 @@
+
+
+nums = [100..999]
+
+isPalindrome [] = True
+isPalindrome [n] = True
+isPalindrome (n:ns)
+  | n == last ns = isPalindrome $ init ns
+  | otherwise = False
+
+largestPdromeProd :: ([Int], Maybe Int) -> Maybe Int
+largestPdromeProd ([], biggest) = biggest
+largestPdromeProd ((a:as), biggest) =
+  let big = largestPdromeProdIn (a, nums, biggest) in
+    largestPdromeProd (as, big `max` biggest)
+
+largestPdromeProdIn :: (Int, [Int], Maybe Int) -> Maybe Int
+largestPdromeProdIn (a, [], biggest) = biggest
+largestPdromeProdIn (a, (b:bs), biggest) = let big = a * b in
+  if isPalindrome $ show $ big
+    then Just big `max` biggest 
+    else largestPdromeProdIn (a, bs, biggest)
+
+main = putStrLn $ show $ largestPdromeProd (nums, Nothing)
