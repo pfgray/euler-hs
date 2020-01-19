567ecad438958531e053f36e516a4c9a7a5c528a
Paul Gray <pfbgray@gmail.com>
2020-01-19 12:19:51 -0500

    Adding solution 6

---
+++ 6/6.hs
@@ -0,0 +1,14 @@
+
+nats = 1 : map (+1) nats
+
+sumOfSquares :: Num t => [t] -> t -> t
+sumOfSquares [] acc = acc
+sumOfSquares (n:ns) acc = sumOfSquares ns ((n*n) + acc ) 
+
+squareOfSum [] acc = acc^2
+squareOfSum (n:ns) acc = squareOfSum ns (n + acc)
+
+hun = take 100 nats
+ten = take 10 nats
+
+main = putStrLn $ show $ ((squareOfSum hun 0) - (sumOfSquares hun 0))
